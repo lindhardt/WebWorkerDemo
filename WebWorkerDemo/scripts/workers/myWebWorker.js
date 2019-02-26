7fddabe3b4f17f5fcb91f5db3ff92032244fbf5f
Jens Lindhardt <jens@lindhardt-freelance.dk>
2019-02-26 10:57:54 +0100

    Created WebWorker demo

---
+++ WebWorkerDemo/scripts/workers/myWebWorker.js
@@ -0,0 +1,19 @@
+﻿function messageHandler(event) {
+    var msg = event.data; // Input data must be JSon
+
+    switch (msg.command) {
+        case "DOWORK":
+            self.postMessage("Doing work " + msg.data);
+            break;
+        case "DOMOREWORK":
+            self.postMessage("Doing more work " + msg.data);
+            break;
+        case "FINISH":
+            self.postMessage("Shutting down " + msg.data);
+            self.close();
+            break;
+    }
+
+}
+
+self.addEventListener("message", messageHandler, false);
